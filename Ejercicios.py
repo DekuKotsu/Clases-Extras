111ee07b37b8cc5325183769bb4bf2cd1953c2f1
DekuKotsu <legendaxel29@gmail.com>
2026-03-19 22:21:41 -0600

    Ejercicios.py

---
+++ Ejercicios.py
@@ -0,0 +1,12 @@
+""" 
+1.-Realizar un ejercicio donde te piden 2 datos de tipo entero y los sumes y posteriormente pero va a
+pedir otro numero el cual va restar esta suma
+
+2.- Realizar un ejercicio donde te piden 2 datos de tipo entero, van a dividirse y despues van a pedir otro 
+dato en el cual va multiplicar el resutado
+
+3.- Realizar como sacar el area y el perimetro de un rectangulo, donde te va a pedir la base y la altura 
+
+4.- Realizar como sacar el area y el perimetro de un triangulo, donde te va a pedir lados para el perimetro 
+y la base y la atura para el area.
+"""
